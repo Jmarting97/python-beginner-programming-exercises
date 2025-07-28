ad832d4e0ab87a97d4c007c50c56788d3ff0c43f
Jmarting97 <jesusmarting97@hotmail.com>
2025-07-28 13:03:59 +0000

    Ejercicios Python completados

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,13 @@
 # ✅↓ Write your code here ↓✅
+def sing():
+    for i in range(4):
+        print("Let it be")
+    
+    print ("there will be an answer")
 
+    for i in range(5):
+        print("Let it be")
+    
+    print("whisper words of wisdom, let it be")
+
+sing()
--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,1 +1,12 @@
 # ✅↓ Write your code here ↓✅
+def number_of_bottles():
+    for i in range (99, 0, -1):
+        if i == 1:
+            print("1 bottle of milk on the wall, 1 bottle of milk.")
+            print("Take one down and pass it around, no more bottles of milk on the wall.")
+        elif i >= 1:
+            print(f"{i} bottles of milk on the wall {i} bottles of milk.")
+    print("No more bottles of milk on the wall, no more bottles of milk.")
+    print("Go to the store and buy some more, 99 bottles of milk on the wall.")
+
+number_of_bottles()      
